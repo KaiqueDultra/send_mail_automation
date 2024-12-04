f56b4f65f27bb102b5c481173d7dee7330038ead
Kaique Barbosa Dultra <kaique.dultra@outlook.com>
2024-12-04 17:16:14 -0300

    Add Outlook API.

--- send_mail.sh
+++ send_mail.sh
@@ -3,10 +3,27 @@
 # Definindo as variáveis
 assunto="Atualização do Agile Release Plan"
 corpo_email="Olá, prezado(a). Por favor, peço que atualize o agile release plan do seu projeto até sexta-feira.\n\nNome do projeto: %s\nLink do projeto: %s\n\nObrigado,\nSE Team."
-from_email="kaique.dultra@outlook.com"
+from_email="kaiquedultra@outlook.com"
+smtp_server="smtp.office365.com"
+smtp_port="587"
+smtp_user="kaiquedultra@outlook.com"
+smtp_pass="$OUTLOOK_APP_PASSWORD"
 
 # Caminho para o arquivo JSON
 json_file="projects.json"
+
+# Criando o arquivo de configuração do smtp
+echo "defaults" > ~/.msmtprc
+echo "auth on" >> ~/.msmtprc
+echo "tls on" >> ~/.msmtprc
+echo "tls_trust_file /etc/ssl/certs/ca-certificates.crt" >> ~/.msmtprc
+echo "smtp $smtp_server" >> ~/.msmtprc
+echo "smtp_port $smtp_port" >> ~/.msmtprc
+echo "from $from_email" >> ~/.msmtprc
+echo "user $smtp_user" >> ~/.msmtprc
+echo "password $smtp_pass" >> ~/.msmtprc
+echo "logfile ~/.msmtp.log" >> ~/.msmtprc
+chmod 600 ~/.msmtprc
 
 # Lendo os arquivos JSON e iterando sobre os projetos
 jq -c '.[]' "$json_file" | while read -r project; do
@@ -20,11 +37,10 @@ jq -c '.[]' "$json_file" | while read -r project; do
     corpo_email_formatado=$(printf "$corpo_email" "$project_name" "$link_confluence")
 
     # Enviando email usando mailx, incluindo o destinatário principal, CC e BCC
-    echo -e "$corpo_email_formatado" | mailx -s "$assunto" \
-        -r "$from_email" \
-        -c "$email_pls" \
-        -b "$email_bcc" \
-        "$email_pls"  # Destinatário principal (To)
+    echo -e "$corpo_email_formatado" | msmtp -s "$assunto" \
+        --cc "$email_pls" \
+        --bcc "$email_bcc"
+        "$email_pls" # Destinatário principal (To)
 
     echo "Email enviado para $email_pls (BCC: $email_bcc)"
 done
