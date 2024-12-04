9dec23ab6dd957983d28a31b3ef19f2523219987
Kaique Barbosa Dultra <kaique.dultra@outlook.com>
2024-12-04 16:59:07 -0300

    script modified.

--- send_mail.sh
+++ send_mail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Definindo as variaveis
+# Definindo as variáveis
 assunto="Atualização do Agile Release Plan"
 corpo_email="Olá, prezado(a). Por favor, peço que atualize o agile release plan do seu projeto até sexta-feira.\n\nNome do projeto: %s\nLink do projeto: %s\n\nObrigado,\nSE Team."
 from_email="kaique.dultra@outlook.com"
@@ -8,26 +8,23 @@ from_email="kaique.dultra@outlook.com"
 # Caminho para o arquivo JSON
 json_file="projects.json"
 
-# Lendo os arquivos JSON e itera sobre os projetos
+# Lendo os arquivos JSON e iterando sobre os projetos
 jq -c '.[]' "$json_file" | while read -r project; do
-    # Extraindo as informacoes do projeto
+    # Extraindo as informações do projeto
     project_name=$(echo "$project" | jq -r '.project_name')
     link_confluence=$(echo "$project" | jq -r '.link_confluence')
     email_pls=$(echo "$project" | jq -r '.email_pls')
-    email_bbc=$(echo "$project" | jq -r '.email_bcc')
+    email_bcc=$(echo "$project" | jq -r '.email_bcc')
 
-    # Substituindo as variaveis no corpo do email
+    # Substituindo as variáveis no corpo do email
     corpo_email_formatado=$(printf "$corpo_email" "$project_name" "$link_confluence")
 
-    # Definindo os destinatarios principais
-    to_email="$email_pls"
-
-    # Enviando email usando o comando mail ou sendmail
+    # Enviando email usando mailx, incluindo o destinatário principal, CC e BCC
     echo -e "$corpo_email_formatado" | mailx -s "$assunto" \
         -r "$from_email" \
-        -t "$to_email" \
         -c "$email_pls" \
-        -b "$email_bcc"
+        -b "$email_bcc" \
+        "$email_pls"  # Destinatário principal (To)
 
     echo "Email enviado para $email_pls (BCC: $email_bcc)"
 done
